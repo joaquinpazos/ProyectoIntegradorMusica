9385047921ef91db67cb32b28abe4a0c8a67cc98
joacopazos <jpazospesquero@udesa.edu.ar>
2020-05-29 16:38:52 -0300

    cambio en el api key

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,8 @@
 window.addEventListener("load",  canciones())
 function canciones(){
-    // api=  "e35e31c137fc27d22a4259a7b0daea9b"
-    var url = "?api_key=e35e31c137fc27d22a4259a7b0daea9b"
+    // https://cors-anywhere.herokuapp.com/https://api.deezer.com/METODO?PARAMETROS
+    // api=  "?api_key=e35e31c137fc27d22a4259a7b0daea9b"
+    var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/METODO?PARAMETROS"
   fetch(url)
   .then(function(response){
     return response.json();
