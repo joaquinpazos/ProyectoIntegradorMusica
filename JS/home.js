411fb5f0c7d80254325fd0cc9e1930318ff271f5
joacopazos <jpazospesquero@udesa.edu.ar>
2020-05-29 17:03:49 -0300

    comente

--- JS/home.js
+++ JS/home.js
@@ -1,11 +1,15 @@
 window.addEventListener("load",  canciones())
+
 function canciones(){
     // https://cors-anywhere.herokuapp.com/https://api.deezer.com/METODO?PARAMETROS
     // api=  "?api_key=e35e31c137fc27d22a4259a7b0daea9b"
-    var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/METODO?PARAMETROS"
+    // top 5 albumes de artistas var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27/top"
+    var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27/top"
   fetch(url)
   .then(function(response){
     return response.json();
   })
   .then(function(data){
     console.log(data);
+  })
+}
