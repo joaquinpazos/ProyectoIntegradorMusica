196775fa7b9783efc2df7b45efda869f1c1eca89
joacopazos <jpazospesquero@udesa.edu.ar>
2020-05-29 14:50:27 -0300

    api

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",  canciones())
 function canciones(){
-    var api=  "e35e31c137fc27d22a4259a7b0daea9b"
-  var url = ""
+    // api=  "e35e31c137fc27d22a4259a7b0daea9b"
+    var url = "?api_key=e35e31c137fc27d22a4259a7b0daea9b"
   fetch(url)
   .then(function(response){
     return response.json();
