a0e560c964952f2175720426a5c54d6dd3edfe06
joacopazos <jpazospesquero@udesa.edu.ar>
2020-05-31 13:13:09 -0300

    nose que cambio es

--- JS/home.js
+++ JS/home.js
@@ -4,7 +4,9 @@ function canciones(){
     // https://cors-anywhere.herokuapp.com/https://api.deezer.com/METODO?PARAMETROS
     // api=  "?api_key=e35e31c137fc27d22a4259a7b0daea9b"
     // top 5 albumes de artistas var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27/top"
-    var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27/top"
+    // top 10 artistas var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists"
+    // top 10 tracks var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks"
+    var url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks"
   fetch(url)
   .then(function(response){
     return response.json();
