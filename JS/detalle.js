89443bab37a6da1cd533c797ced30381a4bd1065
joacopazos <jpazospesquero@udesa.edu.ar>
2020-06-15 17:26:16 -0300

    boton de playlist atroden

--- JS/detalle.js
+++ JS/detalle.js
@@ -22,14 +22,50 @@ window.addEventListener("load", function(){
           li +=    "<p> Title: "+track.title+"</p>"
           li += "</li>"
           ul.innerHTML += li
+
+          document.querySelector("button.boton-favoritos").setAttribute("onclick","agregarPlaylist("+track.id+")")
+
+          //ME FIJO SI LA CANCION ESTA EN MI PLAYLIST CUANDO CARGA LA PAGINA
+          var arrayDePlaylist = JSON.parse(window.sessionStorage.getItem("arrayDePlaylist"))
+          if (arrayDePlaylist != null && arrayDePlaylist.length>0){
+            if (arrayDePlaylist.indexOf(track.id)!= -1) {
+              document.querySelector("button.boton-favoritos").innerText = "Eliminar de Playlist"
+  
+            }
+          }
         })
     }
-
-
-   
-   
-   
-   
-    //cierran la funcion
+      //cierran la funcion
 })
+
+//HAY QUE CHEQUEAR <CUANDO CARGA LA PAGINA> QUE SI LA CANCION YA ESTA EN PLAYLIST. CAMBIO EL BOTON A "ELIMINAR"
+function agregarPlaylist(id) {
+  console.log("clickeaste en la cancion de id: "+id);
+  //PRIMERO reviso si existe el array de cancion cancionesPlaylist
+  var arrayDePlaylist = JSON.parse(window.sessionStorage.getItem("arrayDePlaylist"))
+  if (arrayDePlaylist != null && arrayDePlaylist.length>0) {
+    // console.log("existe y no esta vacio");
+    //antes de agregarlo a favoritos, tengo que verificar si ya esta en el array
+    if (arrayDePlaylist.indexOf(id)!= -1) {
+      document.querySelector("button.boton-favoritos").innerText = "Agregar a Playlist"
+      //ya está como playlist, entonces la borro
+      arrayDePlaylist.splice(arrayDePlaylist.indexOf(id),1)
+      window.sessionStorage.setItem("arrayDePlaylist", JSON.stringify(arrayDePlaylist));
+    } else {
+      document.querySelector("button.boton-favoritos").innerText = "Eliminar de Playlist"
+      // no está como playlist, entonces la agrego en el array
+      arrayDePlaylist.push(id)
+      window.sessionStorage.setItem("arrayDePlaylist", JSON.stringify(arrayDePlaylist));
+    }
+    // console.log(arrayDePlaylist);
+  } else {
+    document.querySelector("button.boton-favoritos").innerText = "Eliminar de Playlist"
+    arrayDePlaylist = []
+    arrayDePlaylist.push(id)
+    window.sessionStorage.setItem("arrayDePlaylist", JSON.stringify(arrayDePlaylist));
+    // console.log("no existe o esta vacio");
+    // console.log(arrayDePlaylist);
+  }
+  console.log(JSON.parse(window.sessionStorage.getItem("arrayDePlaylist")));
+}
 
---
+++ JS/playlist.js
@@ -0,0 +1,38 @@
+window.addEventListener("load",function () {
+    var playlist =  JSON.parse(window.sessionStorage.getItem("arrayDePlaylist"))
+    console.log(playlist.length);
+  
+    for (var i = 0; i < playlist.length; i++) {
+      var id = playlist[i]
+  
+    detalleArtista()
+    function detalleArtista(){
+
+      var urlDetalles = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/"+ id 
+
+      fetch(urlDetalles)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data){
+        console.log(data);
+        var track = data;
+        var ul= document.querySelector("ul.playlist");
+        var li = ""
+        var generos = ""
+          li = "<li>"
+          li +=  "<a href='detalle.html?id="+ id + "'>"
+          li +=    "<p> Title: "+track.title+"</p>"
+          li +=    "</a>"
+          li += "</li>"
+          ul.innerHTML += li
+         }
+        )
+        }
+    }
+}
+)
+
+
+
+
