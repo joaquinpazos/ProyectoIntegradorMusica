b31a18e1bc9c0f8de901ba82d8763502a177843e
joacopazos <jpazospesquero@udesa.edu.ar>
2020-06-15 14:05:34 -0300

    funciona detalle para artistas, no para cancion ni album

--- JS/detalle.js
+++ JS/detalle.js
@@ -3,9 +3,9 @@ window.addEventListener("load", function(){
     var id = urlParams.get('id');
     console.log(id);
     
-    detalleCanciones()
+    detalleArtista()
 
-    function detalleCanciones(){
+    function detalleArtista(){
   
       var urlDetalles = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/"+ id 
 
@@ -15,16 +15,45 @@ window.addEventListener("load", function(){
       })
       .then(function(data6){
         console.log(data6);
-        var cancion = data6;
+        var artista = data6;
         var ul= document.querySelector("ul.lista-detalles");
         var li = ""
         var generos = ""
-          console.log(cancion);
           li = "<li>"
-          li +=    "<p> Title: "+cancion.name+"</p>"
+          li +=    "<p> Title: "+artista.name+"</p>"
           li += "</li>"
           ul.innerHTML += li
         })
     }
+
+})
+
+window.addEventListener("load", function(){
+  var urlParams = new URLSearchParams(window.location.search);
+  var id = urlParams.get('id.track');
+  console.log(id.track);
+  detalleTracks()
+
+  function detalleTracks(){
+
+    var urlDetalles = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/"+ id.track
+
+    fetch(urlDetalles)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data7){
+      console.log(data7);
+      var track = data7;
+      var ul= document.querySelector("ul.lista-detalles");
+      var li = ""
+      var generos = ""
+        console.log(track);
+        li = "<li>"
+        li +=    "<p> Title: "+cancion.name+"</p>"
+        li += "</li>"
+        ul.innerHTML += li
+      })
+  }
 
 })
