598099d41e1a546f5b899a471097764ef4b4a5bc
joacopazos <jpazospesquero@udesa.edu.ar>
2020-06-15 16:44:24 -0300

    detalle de canciones y boton fav

--- JS/detalle.js
+++ JS/detalle.js
@@ -2,12 +2,11 @@ window.addEventListener("load", function(){
     var urlParams = new URLSearchParams(window.location.search);
     var id = urlParams.get('id');
     console.log(id);
-    
-    detalleArtista()
 
+    detalleArtista()
     function detalleArtista(){
   
-      var urlDetalles = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/"+ id 
+      var urlDetalles = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/"+ id 
 
       fetch(urlDetalles)
       .then(function(response){
@@ -15,45 +14,22 @@ window.addEventListener("load", function(){
       })
       .then(function(data6){
         console.log(data6);
-        var artista = data6;
+        var track = data6;
         var ul= document.querySelector("ul.lista-detalles");
         var li = ""
         var generos = ""
           li = "<li>"
-          li +=    "<p> Title: "+artista.name+"</p>"
+          li +=    "<p> Title: "+track.title+"</p>"
           li += "</li>"
           ul.innerHTML += li
         })
     }
 
+
+   
+   
+   
+   
+    //cierran la funcion
 })
 
-window.addEventListener("load", function(){
-  var urlParams = new URLSearchParams(window.location.search);
-  var id = urlParams.get('id.track');
-  console.log(id.track);
-  detalleTracks()
-
-  function detalleTracks(){
-
-    var urlDetalles = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/"+ id.track
-
-    fetch(urlDetalles)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data7){
-      console.log(data7);
-      var track = data7;
-      var ul= document.querySelector("ul.lista-detalles");
-      var li = ""
-      var generos = ""
-        console.log(track);
-        li = "<li>"
-        li +=    "<p> Title: "+cancion.name+"</p>"
-        li += "</li>"
-        ul.innerHTML += li
-      })
-  }
-
-})
